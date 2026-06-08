c9c19a581d5e7e66fec90b6686fe3dd7010b15a2
Mehdi <Mehdi.moosavi88@gmail.com.com>
2026-06-08 12:20:42 +0430

    Implement custom user model with roles

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Marketplace",
+            {
+                "fields": (
+                    "role",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class Roles(models.TextChoices):
+        CUSTOMER = "customer", "Customer"
+        COOK = "cook", "Cook"
+        ADMIN = "admin", "Admin"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.CUSTOMER
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.username
